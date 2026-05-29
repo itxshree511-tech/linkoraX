af07ff7e6bc7735aa6a6d57397f546e2ac4f9971
iTx_sHrEe <itxshree511@gmail.com>
2026-05-30 00:09:34 +0500

    fix railway media

--- linkorax/settings.py
+++ linkorax/settings.py
@@ -161,13 +161,11 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 STATIC_URL = '/static/'
-
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
 
 STATICFILES_STORAGE = (
     'whitenoise.storage.CompressedManifestStaticFilesStorage'
